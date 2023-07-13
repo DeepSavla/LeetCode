a6f5df43248f67d9fe70e5c9ccc85da6b4aa48f8
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2023-07-12 17:24:20 -0700

    Time: 897 ms (57.07%), Space: 22.9 MB (10.29%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -10,16 +10,16 @@ class Solution(object):
             for j in range(i+1,len(prices)):
                 maxDiff = max(maxDiff, prices[j]-prices[i])
         return maxDiff
+        """
         
         maxDiff=0
         i=0
         j=1
         while j<len(prices):
-        
             if  prices[i] < prices[j]:
                 maxDiff = max(maxDiff, prices[j]-prices[i])
             else:
-                i = j
+                i=j
             j+=1
         return maxDiff
         """
@@ -33,6 +33,7 @@ class Solution(object):
                 left = right
             right += 1
         return max_profit
+        """
         
             
               
