96688a44ee882d2921092baa2e40735d8ee34517
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2023-07-18 01:25:35 -0700

    Time: 1906 ms (7.73%), Space: 17.2 MB (89.08%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        hm ={}
+        for s in strs:
+            sortedWord = sorted(s)
+            sortedWord = "".join(sortedWord)
+            if sortedWord in hm.keys():
+                hm[sortedWord].append(s)
+            else:
+                hm[sortedWord] = [s]
+        anagrams = []
+        for key in hm.keys():
+            anagrams.append(hm[key])
+        return anagrams
+            
