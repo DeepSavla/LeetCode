fe65da541fe6dfa491675efdf4478fa825a5c654
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2023-08-29 03:06:54 -0700

    Time: 51 ms (64.12%), Space: 18.8 MB (48.55%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -8,14 +8,10 @@ class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         low=-math.inf
         high=math.inf
-        def validate(node, low, high):
-            if node == None:
+        def isValidNode(node,low,high):
+            if node==None:
                 return True
-            
-            if node.val <= low or node.val >= high:
+            if node.val<=low or node.val>=high:
                 return False
-
-            # The left and right subtree must also be valid.
-            return (validate(node.right, node.val, high) and
-                   validate(node.left, low, node.val))
-        return validate(root,low,high)
+            return isValidNode(node.left,low,node.val) and isValidNode(node.right,node.val,high)
+        return isValidNode(root, low, high)
