7244e933769c6b0aaaf70d00b4f576d1e85c48ad
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2023-10-17 23:27:33 -0700

    Time: 68 ms (49.18%), Space: 211.1 MB (46.93%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
@@ -1,34 +1,48 @@
+# FileName: p2
+# Author: Lebin Zhou
+# CreatTime: 2023-10-09 21:23
+# Description: 
+# Version: 1.0
+
+# frozen_string_literal: true
+
 # Definition for singly-linked list.
-# class ListNode
-#     attr_accessor :val, :next
-#     def initialize(val = 0, _next = nil)
-#         @val = val
-#         @next = _next
-#     end
-# end
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val = 0, _next = nil)
+        @val = val
+        @next = _next
+    end
+end
+
 # @param {ListNode} list1
 # @param {ListNode} list2
 # @return {ListNode}
-def merge_two_lists(list1, list2)
-    result = ListNode.new
-    current = result
-  
-    while list1 && list2
-        if list1.val < list2.val
-            current.next = list1
-            list1 = list1.next
-        else
-            current.next = list2
-            list2 = list2.next
-        end
-        current = current.next
-    end
 
-    if list1 
-        current.next = list1 
-    else 
-        current.next = list2
+def merge_two_lists(list1, list2)
+  result = ListNode.new(-1, nil)
+
+  dummy = result
+  p1, p2 = list1, list2
+
+  while p1 != nil and p2 != nil do
+    if p1.val < p2.val
+      dummy.next = ListNode.new(p1.val, nil)
+      p1 = p1.next
+    else
+      dummy.next = ListNode.new(p2.val, nil)
+      p2 = p2.next
     end
-    
-    result.next
+    dummy = dummy.next
+  end
+
+  if p1 != nil
+    dummy.next = p1
+  end
+
+  if p2 != nil
+    dummy.next = p2
+  end
+
+  return result.next
 end
