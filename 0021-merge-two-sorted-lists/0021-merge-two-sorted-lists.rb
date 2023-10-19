3cd983c8025b081ef9e8a46198e67865ed077ab5
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2023-10-19 02:28:34 -0700

    Time: 78 ms (15.01%), Space: 211 MB (46.65%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
@@ -1,48 +1,18 @@
-# FileName: p2
-# Author: Lebin Zhou
-# CreatTime: 2023-10-09 21:23
-# Description: 
-# Version: 1.0
-
-# frozen_string_literal: true
-
-# Definition for singly-linked list.
-class ListNode
-    attr_accessor :val, :next
-    def initialize(val = 0, _next = nil)
-        @val = val
-        @next = _next
-    end
-end
-
-# @param {ListNode} list1
-# @param {ListNode} list2
-# @return {ListNode}
-
 def merge_two_lists(list1, list2)
-  result = ListNode.new(-1, nil)
+    prehead = ListNode.new(-1)
 
-  dummy = result
-  p1, p2 = list1, list2
+    prev = prehead
 
-  while p1 != nil and p2 != nil do
-    if p1.val < p2.val
-      dummy.next = ListNode.new(p1.val, nil)
-      p1 = p1.next
-    else
-      dummy.next = ListNode.new(p2.val, nil)
-      p2 = p2.next
+    while list1 && list2
+        if list1.val<=list2.val
+            prev.next = list1
+            list1 = list1.next
+        else
+            prev.next = list2
+            list2 = list2.next
+        end
+        prev = prev.next
     end
-    dummy = dummy.next
-  end
-
-  if p1 != nil
-    dummy.next = p1
-  end
-
-  if p2 != nil
-    dummy.next = p2
-  end
-
-  return result.next
+    prev.next = list1.nil?? list2:list1;
+    prehead.next
 end
