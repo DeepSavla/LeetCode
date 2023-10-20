abb73b7c9aff4af2e51a3918a2f67d41250f9283
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2023-10-20 15:33:03 -0700

    Time: 1775 ms (5.14%), Space: 14.5 MB (37.01%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -7,13 +7,11 @@ class Solution(object):
         uniqueChar = set(s)
         freq = set()
         deletions=0
-        print(uniqueChar)
         for setChar in uniqueChar:
             tempCount=0
             for stringChar in s:
                 if setChar == stringChar:
                     tempCount = tempCount + 1
-            print(tempCount)
             if tempCount in freq:
                 while tempCount in freq:
                     tempCount = tempCount-1
