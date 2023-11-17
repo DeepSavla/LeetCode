93a4135b7ce71b379c04fe7aa9e8e3cc6069af96
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2023-11-17 07:54:03 -0800

    Time: 80 ms (14.78%), Space: 16.7 MB (9.95%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hm = {}
+        for char in magazine:
+            if char in hm.keys():
+                hm[char] = hm[char] +1
+            else:
+                hm[char] = 1
+        
+        for char in ransomNote:
+            if char in hm.keys():
+                if hm[char] ==1:
+                    del hm[char]
+                else:
+                    hm[char] = hm[char]-1
+            else:
+                return False
+        return True
