abf3242800045ce694a8ca1aff01e6444e301b38
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2023-11-25 22:32:18 -0800

    Time: 343 ms (82.50%), Space: 13.5 MB (29.80%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        resString = s[0]
+        def expand(i,j):
+            while i>=0 and j<len(s) and s[i] == s[j]:
+                i = i-1
+                j=j+1
+            return s[i+1:j]
+            
+        for i in range(len(s)):
+            evenString = expand(i,i+1)
+            if len(evenString)>len(resString):
+                resString = evenString
+            oddString = expand(i-1,i+1)
+            if len(oddString)>len(resString):
+                resString = oddString
+        return resString
+            
+            
