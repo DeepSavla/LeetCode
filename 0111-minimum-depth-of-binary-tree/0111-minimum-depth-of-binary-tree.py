fb6cbbf3bef3f2309b25fa97cc21c59e3e5b8962
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2023-11-27 22:40:21 -0800

    Time: 445 ms (33.03%), Space: 58.2 MB (39.62%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -4,6 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# class Solution:
+#     def minDepth(self, root: Optional[TreeNode]) -> int:
+#         if root == None:
+#             return 0
+#         if root.left == None and root.right == None:
+#             return 1
+#         elif root.left == None:
+#             rh= self.minDepth(root.right)
+#             lh = math.inf
+#         elif root.right == None:
+#             lh= self.minDepth(root.left)
+#             rh = math.inf
+#         else:
+#             lh= self.minDepth(root.left)
+#             rh= self.minDepth(root.right)
+#         return min(lh,rh)+1
+
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if root == None:
@@ -11,12 +29,9 @@ class Solution:
         if root.left == None and root.right == None:
             return 1
         elif root.left == None:
-            rh= self.minDepth(root.right)
-            lh = math.inf
+            return self.minDepth(root.right)+1
         elif root.right == None:
-            lh= self.minDepth(root.left)
-            rh = math.inf
+            return self.minDepth(root.left)+1
         else:
-            lh= self.minDepth(root.left)
-            rh= self.minDepth(root.right)
-        return min(lh,rh)+1
+            return min(self.minDepth(root.left),self.minDepth(root.right))+1
+        
