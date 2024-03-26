cfe3d014c45df7d77c1be3bedb2b7a890490452a
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-03-25 23:53:33 -0700

    Time: 3621 ms (5.06%), Space: 18.1 MB (58.37%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,39 @@
+import math
+class Solution:
+    def returnHours(self, piles, mid):
+        hours=0
+        if mid>0:
+            for p in piles:
+                hours = hours + math.ceil(p/mid)
+        return hours
+            
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        max=0
+        for dig in piles:
+            if dig>max:
+                max=dig
+        if h==len(piles):
+            return max
+        low=1
+        high=max
+        while low<high:
+            mid = (high + low) // 2
+            hours = self.returnHours(piles, mid)
+            if hours == h:
+                while self.returnHours(piles, mid-1) == h:
+                    mid=mid-1
+                return mid
+            if hours < h:
+                high=mid-1
+            if hours > h:
+                low=mid+1
+        if hours>h:
+            while hours>h:
+                mid=mid+1
+                hours = self.returnHours(piles, mid)
+        while hours<h and self.returnHours(piles, mid-1)<h and mid>1:
+            mid = mid-1
+            hours = self.returnHours(piles, mid)
+        return mid
+    
+                
