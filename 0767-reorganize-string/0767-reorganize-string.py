b08f1b235e22b3e699c9b5aa9cb829fd1fb58893
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-04-04 03:40:34 -0700

    Time: 42 ms (25.85%), Space: 16.6 MB (77.74%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -7,25 +7,23 @@ class Solution:
                 hm[letter]=1
             else:
                 hm[letter] = hm[letter] +1
-        resArr=[]
+        resStr= ""
         prevChar = None
         while len(hm) > 0:
             maxFreq = heapq.nlargest(2,hm.keys(),key=hm.get)
             if len(hm)==1 and hm[maxFreq[0]]>1: #for null string case
                 return ""
-            if prevChar == None:
+            if prevChar == None:    #for first case
                 currChar = maxFreq[0]
             else:
                 if maxFreq[0] == prevChar:
                     currChar = maxFreq[1]
                 else:
                     currChar = maxFreq[0]
-            resArr.append(currChar)
+            resStr = resStr + currChar
             prevChar = currChar
             if hm[currChar] > 1:
                 hm[currChar] = hm[currChar]-1
             else:
                 del hm[currChar]
-        resStr =""
-        resStr = resStr.join(resArr)
         return resStr
