674c9e8a6b8f78afeeac7dfee7d330d8a2732a95
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-04-04 03:38:15 -0700

    Time: 27 ms (97.71%), Space: 16.6 MB (77.74%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -8,22 +8,26 @@ class Solution:
             else:
                 hm[letter] = hm[letter] +1
         resArr=[]
+        prevChar = None
         while len(hm) > 0:
             maxFreq = heapq.nlargest(2,hm.keys(),key=hm.get)
             if len(hm)==1 and hm[maxFreq[0]]>1: #for null string case
                 return ""
-            if len(resArr) == 0:
-                resArr.append(maxFreq[0])
-                prevChar = maxFreq[0]
-                if hm[maxFreq[0]] > 1:
-                    hm[maxFreq[0]] = hm[maxFreq[0]]-1
-                else:
-                    del hm[maxFreq[0]]
-                continue    #imp
-            if maxFreq[0] == prevChar:
-                currChar = maxFreq[1]
-            else:
+            # if len(resArr) == 0:
+            #     resArr.append(maxFreq[0])
+            #     prevChar = maxFreq[0]       #storing last used char so not used again
+            #     if hm[maxFreq[0]] > 1:
+            #         hm[maxFreq[0]] = hm[maxFreq[0]]-1
+            #     else:
+            #         del hm[maxFreq[0]]
+            #     continue    #imp
+            if prevChar == None:
                 currChar = maxFreq[0]
+            else:
+                if maxFreq[0] == prevChar:
+                    currChar = maxFreq[1]
+                else:
+                    currChar = maxFreq[0]
             resArr.append(currChar)
             prevChar = currChar
             if hm[currChar] > 1:
@@ -33,4 +37,3 @@ class Solution:
         resStr =""
         resStr = resStr.join(resArr)
         return resStr
-            #check previous character for empty string
