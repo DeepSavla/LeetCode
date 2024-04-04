35140981f85d25d265e1dd937f074f7b5e8eea88
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-04-04 03:42:00 -0700

    Time: 49 ms (5.81%), Space: 16.8 MB (9.18%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -7,7 +7,7 @@ class Solution:
                 hm[letter]=1
             else:
                 hm[letter] = hm[letter] +1
-        resArr=[]
+        resStr= ""
         prevChar = None
         while len(hm) > 0:
             maxFreq = heapq.nlargest(2,hm.keys(),key=hm.get)
@@ -20,12 +20,10 @@ class Solution:
                     currChar = maxFreq[1]
                 else:
                     currChar = maxFreq[0]
-            resArr.append(currChar)
+            resStr = resStr + currChar
             prevChar = currChar
             if hm[currChar] > 1:
                 hm[currChar] = hm[currChar]-1
             else:
                 del hm[currChar]
-        resStr =""
-        resStr = resStr.join(resArr)
         return resStr
