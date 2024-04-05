c98f76a537968d4ae91469385ebd44d41477edbf
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-04-05 04:11:56 -0700

    Time: 224 ms (37.60%), Space: 18.9 MB (76.42%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        satisfiedCustCount = 0
+        for i in range(len(grumpy)):
+            if grumpy[i] == 0:
+                satisfiedCustCount = satisfiedCustCount + customers[i]
+        start = minutes
+        for i in range(minutes):
+            if grumpy[i] == 1:
+                satisfiedCustCount = satisfiedCustCount + customers[i]
+        maxsatisfiedCustCount = satisfiedCustCount
+        while start < len(grumpy):
+            if grumpy[start] == 1:
+                satisfiedCustCount = satisfiedCustCount + customers[start]
+            if grumpy[start-minutes] == 1:
+                    satisfiedCustCount = satisfiedCustCount - customers[start-minutes]
+            start=start+1
+            maxsatisfiedCustCount = max(maxsatisfiedCustCount, satisfiedCustCount)
+        return maxsatisfiedCustCount
+                    
+            
