72d50a15b894ce95d6810b725cbf8f2678939da8
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-04-30 18:25:24 -0700

    Time: 90 ms (60.82%), Space: 18.3 MB (77.17%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,4 +1,10 @@
 class Solution:
+    def DFS(self, node, visited, adj):
+            visited[node] =True
+            for n in adj[node]:
+                if visited[n] == False:
+                    self.DFS(n,visited, adj)
+    
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         adj = []
         visited = [False]* n
@@ -10,16 +16,10 @@ class Solution:
             adj[edge[0]].append(edge[1])
             adj[edge[1]].append(edge[0])
         #adj created
-        def DFS(node):
-            visited[node] =True
-            for n in adj[node]:
-                if visited[n] == False:
-                    DFS(n)
-        
         for i in range(len(adj)):
             if visited[i] == False:
                 components +=1
-                DFS(i)
+                self.DFS(i,visited,adj)
         return components
         
                 
