6108ba2aa89725e3635d3b18b0cf609a399d674e
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-05-08 23:58:44 -0700

    Time: 121 ms (26.40%), Space: 17.1 MB (72.50%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,7 +3,7 @@ class Solution:
         visitedCell = set()
         q = deque()
         area = 0
-        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+        directions = [[0,1],[0,-1],[1,0],[-1,0]] 
         # find all 1 in matrix that are not visited and perform BFS in its 4 directions
         for i in range(len(grid)):
             for j in range(len(grid[i])):
@@ -17,7 +17,7 @@ class Solution:
                         for d in directions:
                             x = s[0]+d[0]
                             y = s[1]+d[1]
-                            if (x in range(len(grid)) and y in range(len(grid[0]))) and (x,y) not in visitedCell and grid[x][y] == 1:
+                            if (x in range(len(grid)) and y in range(len(grid[0]))) and (x,y) not in visitedCell and grid[x][y] == 1: #understand conditions
                                 q.append([x,y])
                                 visitedCell.add((x,y))
                     area = max(area,curArea)
