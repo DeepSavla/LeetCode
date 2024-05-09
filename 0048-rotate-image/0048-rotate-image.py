b023163c7f1d5330ab0a86bbc58f99d5e17ece16
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-05-09 02:44:36 -0700

    Time: 34 ms (82.67%), Space: 16.6 MB (19.83%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -5,14 +5,12 @@ class Solution:
         while top<=bottom:
             left = top
             right = bottom
-            for i in range(right-left): #
+            for i in range(right-left): #coz addition and substraction we want from 0 to number ele in current square row minus 1
                 temp = matrix[top][left+i]
                 matrix[top][left+i] = matrix[bottom-i][left]
                 matrix[bottom-i][left]= matrix[bottom][right-i]
                 matrix[bottom][right-i]= matrix[top+i][right]
                 matrix[top+i][right] =temp
             top +=1
-            bottom-=1
-                
-            
-        
+            bottom-=1 
+# for explanation refer https://www.youtube.com/watch?v=fMSJSS7eO1w
