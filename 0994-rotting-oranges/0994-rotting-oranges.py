5a359a30d57d338572a80584c66a2e712fbe0fbd
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-05-12 18:33:24 -0700

    Time: 60 ms (10.04%), Space: 16.7 MB (8.60%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,6 +2,8 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         visited = set()
         q = deque()
+        fresh = 0
+        rotted = 0
         minutes = 0
         count=0
         rows = len(grid)
@@ -9,9 +11,11 @@ class Solution:
         neighbors = [(0,1),(0,-1),(1,0),(-1,0)]
         for i in range(rows):
             for j in range(columns):
-                if grid[i][j]==2 and (i,j) not in visited:
+                if grid[i][j]==2:
                     q.append([i,j])
                     count+=1
+                if grid[i][j]==1:
+                    fresh+=1
         while len(q)!=0:
             freshFound = False
             iterations = count
@@ -24,16 +28,15 @@ class Solution:
                     if (x in range(rows) and y in range(columns)) and (x,y) not in visited and grid[x][y]==1:
                         q.append([x,y])
                         grid[x][y]=2
+                        rotted +=1
                         count+=1
                         freshFound = True
                         visited.add((x,y))
             if freshFound: #checking if there was any fresh in neighbors
                 minutes+=1
-        for i in range(rows):
-            for j in range(columns):
-                if grid[i][j] ==1:
-                    return -1
-        return minutes
+        if fresh == rotted:
+            return minutes
+        return -1
         
                         
                     
