d3d1c5503c35dd5a33327bf92802a90e464e5190
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-05-12 18:15:57 -0700

    Time: 49 ms (64.27%), Space: 16.6 MB (25.10%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,71 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        # visited = set()
+        # q = deque()
+        # minutes = 0
+        # rows = len(grid)
+        # columns = len(grid[0])
+        # neighbors = [(0,1),(0,-1),(1,0),(-1,0)]
+        # for i in range(rows):
+        #     for j in range(columns):
+        #         if grid[i][j]==2 and (i,j) not in visited:
+        #             q.append([i,j])
+        #             while len(q)!=0:
+        #                 s=q.popleft()
+        #                 freshFound = False
+        #                 for index in neighbors:
+        #                     x = index[0]+s[0]
+        #                     y = index[1]+s[1]
+        #                     if (x in range(rows) and y in range(columns)) and (x,y) not in visited and grid[x][y]==1:
+        #                         q.append([x,y])
+        #                         grid[x][y]=2
+        #                         freshFound = True
+        #                         visited.add((x,y))
+        #                 if freshFound:
+        #                     minutes+=1
+        # for i in range(rows):
+        #     for j in range(columns):
+        #         if grid[i][j] ==1:
+        #             return -1
+        # return minutes
+        # for working for simultaneous rottening from multiple oranges
+        
+        visited = set()
+        q = deque()
+        minutes = 0
+        count=0
+        rows = len(grid)
+        columns = len(grid[0])
+        neighbors = [(0,1),(0,-1),(1,0),(-1,0)]
+        for i in range(rows):
+            for j in range(columns):
+                if grid[i][j]==2 and (i,j) not in visited:
+                    q.append([i,j])
+                    count+=1
+        while len(q)!=0:
+            freshFound = False
+            iterations = count
+            count = 0
+            for c in range(iterations):
+                s = q.popleft()
+                for index in neighbors:
+                    x = index[0]+s[0]
+                    y = index[1]+s[1]
+                    if (x in range(rows) and y in range(columns)) and (x,y) not in visited and grid[x][y]==1:
+                        q.append([x,y])
+                        grid[x][y]=2
+                        count+=1
+                        freshFound = True
+                        visited.add((x,y))
+            if freshFound:
+                minutes+=1
+        for i in range(rows):
+            for j in range(columns):
+                if grid[i][j] ==1:
+                    return -1
+        return minutes
+                    
+#[[2,1,1],[1,1,1],[0,1,2]]
+        
+                        
+                    
