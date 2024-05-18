7c5b526b342897f97d2318bf8e31e3d60a1d2ba5
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-05-17 20:52:59 -0700

    Time: 38 ms (37.18%), Space: 16.6 MB (17.99%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,33 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        rows = len(matrix)
+        columns = len(matrix[0])
+        directions = {0:[0,1],1:[1,0],2:[0,-1],3:[-1,0]}
+        direction = 0
+        visited = set()
+        cell = [0,0]
+        res = [matrix[0][0]]
+        visited.add((0,0))
+        while(1):
+            added = False
+            nextRow = cell[0] + directions[direction][0]
+            nextColumn = cell[1] + directions[direction][1]
+            if nextRow in range(rows) and nextColumn in range(columns) and (nextRow,nextColumn) not in visited:
+                visited.add((nextRow,nextColumn))
+                res.append(matrix[nextRow][nextColumn])
+                cell = [nextRow,nextColumn]
+                added = True
+            else:
+                direction = (direction+1)%4
+                nextRow = cell[0] + directions[direction][0]
+                nextColumn = cell[1] + directions[direction][1]
+                if nextRow in range(rows) and nextColumn in range(columns) and (nextRow,nextColumn) not in visited:
+                    visited.add((nextRow,nextColumn))
+                    res.append(matrix[nextRow][nextColumn])
+                    cell = [nextRow,nextColumn]
+                    added = True
+            if added == False:
+                break
+        return res
+                
+                
