809e9db56cce3c7f347e158cde60eb5031c37fe8
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-06-02 01:45:23 -0700

    Time: 55 ms (76.19%), Space: 16.6 MB (74.32%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -6,13 +6,36 @@ class Solution:
         winLen = len(s1)
         i=0
         j=winLen-1
-        sortedStr = sorted(s1)
-        while j<len(s2):
-            if sortedStr == sorted(s2[i:j+1]):
+        hm1 = {}
+        hm2 ={}
+        if winLen>len(s2):
+            return False
+        for idx in s1:
+            if idx in hm1:
+                hm1[idx] +=1
+            else:
+                hm1[idx]=1
+        for idx in range(j+1):
+            if s2[idx] in hm2:
+                hm2[s2[idx]]+=1
+            else:
+                hm2[s2[idx]]=1
+        if hm1==hm2:
                 return True
+        while j<len(s2)-1:
+            hm2[s2[i]] -=1
+            if hm2[s2[i]]==0:
+                del hm2[s2[i]]
             i+=1
             j+=1
+            if s2[j] in hm2:
+                hm2[s2[j]] +=1
+            else:
+                hm2[s2[j]]=1
+            if hm1==hm2:
+                return True
         return False
+            
             
         
         
