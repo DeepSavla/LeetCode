f4ff7be928bdac0f279017a66c190e1518efd3ea
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-06-04 03:56:07 -0700

    Time: 49 ms (21.18%), Space: 16.6 MB (76.67%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n==1:
+            return "1"
+        if n==2:
+            return "11"
+        curStr = "11"
+        count = 0
+        for i in range(3,n+1):
+            newStr=""
+            for j in range(len(curStr)):
+                if j!=len(curStr)-1 and curStr[j] == curStr[j+1]:
+                    count+=1
+                else:
+                    count+=1
+                    newStr = newStr + str(count) + curStr[j]
+                    count = 0
+            curStr = newStr
+        return str(curStr)
+                
+            
+        
