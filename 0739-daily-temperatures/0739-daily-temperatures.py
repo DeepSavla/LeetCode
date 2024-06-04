718884d2d21269974335d73a58bbbd2d9d5bf185
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-06-03 22:48:45 -0700

    Time: 922 ms (20.63%), Space: 32.9 MB (11.12%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,12 @@
+#https://www.youtube.com/watch?v=cTBiBSnjO3c&t=611s
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res =[0] * len(temperatures)
+        tempStack =[[temperatures[0],0]]
+        for i in range(1,len(temperatures)):
+            if temperatures[i]>tempStack[-1][0]:
+                while len(tempStack)>0 and temperatures[i]>tempStack[-1][0]:
+                    res[tempStack[-1][1]] = i - tempStack[-1][1]
+                    tempStack.pop()
+            tempStack.append([temperatures[i],i])
+        return res
