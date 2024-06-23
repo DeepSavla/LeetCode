9e486d26ef223c576a53fdf39042f4874d00e3a1
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-06-22 19:03:48 -0700

    Time: 67 ms (91.90%), Space: 19.7 MB (89.78%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,15 +1,5 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # intervals.append(newInterval)
-        # intervals = sorted(intervals)
-        # resInt = [intervals[0]]
-        # for i in range(1,len(intervals)):
-        #     if intervals[i][0] <= resInt[-1][1]:
-        #         if intervals[i][1] > resInt[-1][1]:
-        #             resInt[-1][1] = intervals[i][1]
-        #     else:
-        #         resInt.append(intervals[i])
-        # return resInt
         if not len(intervals):
             return [newInterval]
         resInt = []
@@ -19,7 +9,6 @@ class Solution:
                 break
             if i==len(intervals)-1:
                 intervals.append(newInterval)
-        print(intervals)
         resInt = [intervals[0]]
         for i in range(1,len(intervals)):
             if intervals[i][0] <= resInt[-1][1]:
