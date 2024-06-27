839d962558f11881c41f19bbe7b660cff0616570
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-06-27 00:04:54 -0700

    Time: 24 ms (99.05%), Space: 16.6 MB (51.40%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -6,17 +6,16 @@ class Solution:
                 hm[l] +=1
             else:
                 hm[l] =1
-        odd = 1
         totalLen = 0
+        oddEncountered = False
         for k in hm.keys():
             if hm[k]%2==0:
                 totalLen = totalLen + hm[k]
             else:
-                if odd ==1:
-                    totalLen = totalLen + hm[k]
-                    odd=0
-                else:
                     totalLen = totalLen + hm[k] -1
+                    oddEncountered = True
+        if oddEncountered:
+            return totalLen+1
         return totalLen
                 
             
