eb5fa88c83eb418d2adf379eef5cedf9dd211efa
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-07-12 01:38:33 -0700

    Time: 92 ms (25.77%), Space: 18.4 MB (46.18%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -7,9 +7,7 @@ class Solution:
         for e in edges:
             adj[e[0]].append(e[1])
             adj[e[1]].append(e[0])
-        print(adj)
         def dfs(cur,parent):
-            print(cur)
             visited[cur] = True
             for i in adj[cur]:
                 if visited[i] ==False:
@@ -21,7 +19,6 @@ class Solution:
             return True
         if dfs(0,-1) == False:
             return False
-        print(visited)
         for v in visited:
             if v==False:
                 return False
