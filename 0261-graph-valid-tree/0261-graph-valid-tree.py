d9269f43cac0f54e92de2e38f385a371ad92508a
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-07-12 01:38:12 -0700

    Time: 92 ms (25.77%), Space: 18.4 MB (46.18%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,28 +1,30 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        adj = []
-        visited = [False] *n
+        adj =[]
+        visited = [False] * n
         for i in range(n):
             adj.append([])
         for e in edges:
             adj[e[0]].append(e[1])
             adj[e[1]].append(e[0])
-        def dfsTraverse(source,parent):
-            visited[source]=True
-            for n in adj[source]:
-                if visited[n] ==False:
-                    if dfsTraverse(n,source):
-                        return True
+        print(adj)
+        def dfs(cur,parent):
+            print(cur)
+            visited[cur] = True
+            for i in adj[cur]:
+                if visited[i] ==False:
+                    if dfs(i,cur) == False:
+                        return False
                 else:
-                    if n!=parent:
-                        return True
+                    if i!=parent:
+                        return False
+            return True
+        if dfs(0,-1) == False:
             return False
-        
-        if dfsTraverse(i,-1)==True:
-                    return False
-        for i in range(n):
-            if visited[i]==False:
+        print(visited)
+        for v in visited:
+            if v==False:
                 return False
         return True
-            
+                
         
