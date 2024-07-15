313ab325d5c9c9f28e30d6dfcbc4178f4912575c
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-07-14 22:01:26 -0700

    Time: 54 ms (19.17%), Space: 16.5 MB (36.22%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,40 +2,12 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         res = 0
         numLen = len(s)
-        i=0
         hm ={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        while i<numLen:
-            if s[i]=="I" and i<numLen-1:
-                if s[i+1] == "V":
-                    res = res+4
-                    i+=2
-                elif s[i+1] == "X":
-                    res = res+9
-                    i+=2
-                else:
-                    res = res+hm[s[i]]
-                    i+=1
-            elif s[i]=="X" and i<numLen-1:
-                if s[i+1] == "L":
-                    res = res+40
-                    i+=2
-                elif s[i+1] == "C":
-                    res = res+90
-                    i+=2
-                else:
-                    res = res+hm[s[i]]
-                    i+=1
-            elif s[i]=="C" and i<numLen-1:
-                if s[i+1] == "D":
-                    res = res+400
-                    i+=2
-                elif s[i+1] == "M":
-                    res = res+900
-                    i+=2
-                else:
-                    res = res+hm[s[i]]
-                    i+=1
+        #trick: if current roman is smaller than the next roman character, then we need to substract that value from next character. Else, add it
+        for i in range(numLen):
+            if i+1!=numLen and hm[s[i+1]] > hm[s[i]]:
+                res = res-hm[s[i]]
             else:
                 res = res+hm[s[i]]
-                i+=1
+        print(res)
         return res
