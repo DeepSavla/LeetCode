23f36801b029f438646e3ceb180cb52add1b65d0
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-07-19 22:27:41 -0700

    Time: 37 ms (66.04%), Space: 16.6 MB (49.40%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,29 +1,30 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        res = 0
-        sign = 1
-        i=0
+        ans  = 0
+        isPositive = True
+        i = 0
         if s=="":
             return 0
-        while i<len(s) and s[i]==" ":
-            i+=1
-        if i<len(s) and s[i] in "+-":
-            if s[i] =="+":
-                sign = 1
-            else:
-                sign = -1
-            i+=1
-        for j in range(i,len(s)):
-            if s[j]==" " or s[j] in "+-":
-                break
-            elif ord(s[j])>=48 and ord(s[j])<=57:
-                res = res*10 + (ord(s[j])-48)
-            else:
-                break
-        res = res*sign
-        if res>(2**31-1):
-            res = 2**31-1
-        if res< -2**31:
-            res = -2**31
-        return res
+        while i<len(s) and s[i] == ' ':
+            i += 1
         
+        if i<len(s) and s[i] in '+-':
+            if s[i] == "-":
+                isPositive = False
+            i+=1
+        
+        
+        while i<len(s) and ord(s[i]) in range(48, 58):
+            x = ord(s[i]) - 48
+            ans = ans*10 + x 
+            i += 1
+            if i == len(s):
+                break
+                
+        if isPositive == False:
+            ans= -ans
+        if ans > 2**31 -1:
+            ans = 2**31 -1
+        if ans < -2**31:
+            ans = -2**31
+        return ans
