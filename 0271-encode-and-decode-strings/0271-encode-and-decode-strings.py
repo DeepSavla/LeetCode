9b858c7f51464374c1cb48a1e6c133085aa72cde
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-07-28 03:16:07 -0700

    Time: 114 ms (13.45%), Space: 17.1 MB (17.56%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,13 +2,32 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        return 'π'.join(strs)
+        enStr = ''
+        for i in range(len(strs)):
+            strs[i] = strs[i].replace('/', '//')
+            enStr += strs[i] + '/:'
+        return enStr
         
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        return s.split('π')
+        res = []
+        j = 0
+        curr = ''
+        while j<len(s):
+            if s[j] != '/':
+                curr += s[j]
+                j += 1
+            else:
+                if s[j + 1] == '/':
+                    curr += '/'
+                else: 
+                    res.append(curr)
+                    curr = ''
+                j += 2
+
+        return res
         
 
 
