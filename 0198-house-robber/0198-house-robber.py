652b8b4342ab20c800792f0db356e1baf47f10e0
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-08-16 02:40:48 -0700

    Time: 31 ms (85.32%), Space: 16.5 MB (66.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,5 +4,9 @@ class Solution:
         if len(nums)>2:
             total[2] = nums[2]+total[0]
         for i in range(3,len(nums)):
-            total[i] = max(total[i-3],total[i-2])+nums[i]
+            prev1=0
+            if i-3>=0:
+                prevTotal1 = total[i-3]
+            prevTotal2 = total[i-2]
+            total[i] = max(prevTotal1,prevTotal2)+nums[i]
         return max(total)
