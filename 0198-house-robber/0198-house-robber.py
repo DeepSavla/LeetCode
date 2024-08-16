8bd7ffc68f4f103680bb1630bd770ab6bd6acc2d
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-08-16 01:44:14 -0700

    Time: 42 ms (16.47%), Space: 16.5 MB (66.48%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        total = nums.copy()
+        for i in range(2,len(nums)):
+            prevMax=0
+            for j in range(i-1):
+                prevMax = max(prevMax,total[j])
+            total[i] = prevMax + nums[i]
+        return max(total)
+                
+                
+        
