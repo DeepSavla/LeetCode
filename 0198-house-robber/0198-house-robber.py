b125c92589555f5effb8bf1b7eeae48b7a5875d4
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-08-16 02:46:47 -0700

    Time: 33 ms (76.14%), Space: 16.5 MB (66.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         total = nums.copy()
-        if len(nums)>2:
-            total[2] = nums[2]+total[0]
-        for i in range(3,len(nums)):
-            prev1=0
+        for i in range(2,len(nums)):
+            prevTotal1=0
             if i-3>=0:
                 prevTotal1 = total[i-3]
             prevTotal2 = total[i-2]
