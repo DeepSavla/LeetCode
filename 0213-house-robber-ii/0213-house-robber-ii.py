93fed67b589bed4ab3edeeb27ce85e5365c60fdd
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-08-21 17:57:05 -0700

    Time: 44 ms (7.95%), Space: 16.5 MB (67.84%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -7,14 +7,11 @@ class Solution:
                 for j in range(i-1):
                     maxVal = max(maxVal, totalTillNow[j])
                 totalTillNow[i] = maxVal + arr[i]
-            print(totalTillNow)
             return max(totalTillNow)
         
         if len(nums)==1:
             return nums[0]
         t1 = getMaxTotal(nums[:len(nums)-1:])
         t2 = getMaxTotal(nums[1::])
-        print(t1)
-        print(t2)
         return max(t1,t2)
         
