15020c034c9fd5ef41f250d76e3bf8f5add6a2cd
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-09-16 01:03:50 -0700

    Time: 33 ms (76.13%), Space: 16.5 MB (52.14%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -6,9 +6,8 @@ class Solution:
                 memo[i] = 0
             else:
                 memo[i] = memo[i+1]
-            if s[i] in "12":
-                if i+1<len(s) and (s[i] =="1" or (s[i]=="2" and s[i+1] in "0123456")):
-                    memo[i]= memo[i] + memo[i+2]
+            if i+1<len(s) and (s[i] =="1" or (s[i]=="2" and s[i+1] in "0123456")):
+                memo[i]= memo[i] + memo[i+2]
         return memo[0]
                                    
                                   
