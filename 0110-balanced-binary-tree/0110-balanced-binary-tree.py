93e95202883f5f45b6d5d6635c5edd59884a81bb
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-09-22 21:27:39 -0700

    Time: 59 ms (5.81%), Space: 17.7 MB (23.19%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -7,17 +7,16 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def getHeight(node):
-            if node==None:
-                return 1
+            if node == None:
+                return 0
             lh = getHeight(node.left)
             rh = getHeight(node.right)
             return max(lh,rh)+1
-        
+    
         if root==None:
             return True
-        if abs(getHeight(root.left) - getHeight(root.right))>1:
+        if abs(getHeight(root.left) - getHeight(root.right)) > 1:
             return False
-        if root.left and root.right:
+        else:
             return self.isBalanced(root.left) and self.isBalanced(root.right)
-        return True
         
