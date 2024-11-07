67e6502b10b180de74973716524a238e8bb7de7d
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-11-06 19:32:35 -0800

    Time: 7 ms (60.08%), Space: 17.8 MB (43.19%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -6,14 +6,16 @@ class Solution:
             # Process collisions
             #checking only positive in stack bec 
             #negative before positive will go in left without coliding
-            while len(s)>0 and s[-1] > 0 and a < 0:
-                if  abs(a)>abs(s[-1]):
-                    s.pop()  # Pop smaller asteroid from stack
-                    continue
-                elif abs(s[-1]) == abs(a):
-                    s.pop()  # Both asteroids explode
-                break
-            else:
-                # If no collision, add the asteroid to the stack
-                s.append(a)
+            while 1:
+                if len(s)>0 and s[-1] > 0 and a < 0:
+                    if  abs(a)>abs(s[-1]):
+                        s.pop()  # Pop smaller asteroid from stack
+                        continue
+                    elif abs(s[-1]) == abs(a):
+                        s.pop()  # Both asteroids explode
+                    break
+                else:
+                    # If no collision, add the asteroid to the stack
+                    s.append(a)
+                    break
         return s
