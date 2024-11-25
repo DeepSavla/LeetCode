b98730a4ffad4e4539494e4fc2d525087158ac21
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-11-25 02:58:40 -0800

    Time: 86 ms (31.15%), Space: 16.6 MB (95.82%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.py
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.py
@@ -1,14 +1,19 @@
 class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
-        for i in range(len(nums)-k-k+1):
-            found = None
-            for j in range(i+1, i+k):
-                if nums[j]<=nums[j-1]:
-                    found = False
-            for j in range(i+k+1,i+k+k):
-                if nums[j]<=nums[j-1]:
-                    found = False
-            if found != False:
-                return True
+        s = set() #to note indexes of increasing sunArr of length k
+        curIncreasingCount = 1 #initial length is 1
+        for i in range (1,len(nums)):
+            if nums[i]>nums[i-1]:
+                curIncreasingCount +=1
+                if curIncreasingCount>=k:
+                    if i-k-k+1 in s:
+                        return True
+                    else:
+                        s.add(i-k+1)
+            else:
+                curIncreasingCount = 1
+        if k == 1: #handling for case where k=1
+            return True
+            
                     
         
