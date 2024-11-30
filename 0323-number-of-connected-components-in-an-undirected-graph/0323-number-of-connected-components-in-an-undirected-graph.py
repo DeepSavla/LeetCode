e25ba784e3df845785d051bf0f09bc2e7e1468ab
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-11-29 22:06:33 -0800

    Time: 7 ms (52.53%), Space: 18.7 MB (13.02%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,30 +1,32 @@
 class Solution:
-    def DFS(self, node, visited, adj):
-            visited[node] =True
-            for n in adj[node]:
-                if visited[n] == False:
-                    self.DFS(n,visited, adj)
-    
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         adj = []
-        visited = [False]* n
-        components=0
-        #creating adj
         for i in range(n):
             adj.append([])
-        for edge in edges:
-            adj[edge[0]].append(edge[1])
-            adj[edge[1]].append(edge[0])
-        #adj created
-        #running DFS on sach node of graph if not visited
-        for i in range(len(adj)):
+        for e in edges:
+            adj[e[0]].append(e[1])
+            adj[e[1]].append(e[0])
+        visited = [False] * n
+        connectedComponents = 0
+        
+        def DFS(n, visited):
+            for node in adj[n]:
+                if visited[node] == False:
+                    visited[node] = True
+                    DFS(node,visited)
+        
+        for i in range(n):
             if visited[i] == False:
-                components +=1
-                self.DFS(i,visited,adj)
-        return components
+                connectedComponents +=1
+                visited[i] = True
+                DFS(i,visited)
+        return connectedComponents
         
-                
-        
-    
-            
-            
+'''
+- Using DFS
+- create adjcency list using the edges
+- Create a visited array to track nodes visited through each DFS call
+- for all the nodes if they are not visited, perform recursive DFS
+- DFC at outer level will be performed the number of times as the number of disconnected components (connected nodes will be visited through the internal DFS)
+
+'''
