d7bc1b9a1e354f9a6dfaaa2614a4ec851e2a12a6
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-12-01 19:35:59 -0800

    Time: 5 ms (59.40%), Space: 18.7 MB (11.77%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,31 +1,34 @@
+#detect cycle using DFS
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0] * numCourses
-        adjMat =[]
+        adjList = []
         for i in range(numCourses):
-            adjMat.append([])
-        for edge in prerequisites:
-            adjMat[edge[1]].append(edge[0])
-        for i in adjMat:
-            for j in i:
-                indegree[j] += 1
-        # till now first created adjMatrix followed by indegree of nodes
-        q= deque()
-        for i in range(len(indegree)):
-            if indegree[i] == 0:
-                q.append(i)
-        if len(q)==0:
-            return False
-        countQNodes = 0         #for cycle
-        while len(q)!=0:
-            s = q.popleft()
-            for n in adjMat[s]:
-                indegree[n] -= 1
-                if indegree[n] == 0:
-                    q.append(n)
-            countQNodes +=1     #counting number of Nodes processed in queue
-        if countQNodes == numCourses : #if all nodes have become 0 and entered queue
-            return True
-        else:
-            return False
+            adjList.append([])
+        visited = [False]*numCourses
+        cycleDetection = False
+        for p in prerequisites:
+            adjList[p[1]].append(p[0])
+        cycleSet = set()
+        cycleSet.add(0)
+        def DFS(s):
+            nonlocal cycleDetection
+            cycleSet.add(s)
+            for node in adjList[s]:
+                if visited[node] == False:
+                    visited[node] = True
+                    DFS(node)
+                elif node in cycleSet:
+                    cycleDetection = True
+                    return
+            cycleSet.remove(s)
             
+        
+        for i in range(numCourses):
+            if visited[i] == False:
+                visited[i] = True
+                DFS(i)
+        if cycleDetection == True:
+            return False
+        return True
+        
+        
