74ed62b7e6630631823f977e225d41c3bd4d3496
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-12-04 20:43:43 -0800

    Time: 316 ms (8.07%), Space: 27.8 MB (9.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def BFS(r,c):
-            q = deque()
-            q.append([r,c])
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
-            while len(q)!=0:
-                curCell = q.popleft()
-                for d in directions:
-                    row = curCell[0] + d[0]
-                    col = curCell[1] + d[1]
-                    if (row in range(rows) and col in range(columns)) and (row,col) not in visitedCells and grid[row][col] =='1':
-                        q.append([row,col])
-                        visitedCells.add((row,col))
+        visited = set()
+        numberOfIslands = 0
+        neighbours = [[0,1],[0,-1],[1,0],[-1,0]]
         
-        #main code
-        rows = len(grid)
-        columns = len(grid[0])
-        visitedCells = set()
-        totalIslands = 0
-        for r in range(rows):
-            for c in range(columns):
-                if grid[r][c] == '1' and (r,c) not in visitedCells:
-                    totalIslands+=1
-                    visitedCells.add((r,c))
-                    BFS(r,c)
-        return totalIslands
+        def DFS(i,j):
+            for n in neighbours:
+                row = i+n[0]
+                col = j+n[1]
+                if (row,col) not in visited and row in range(len(grid)) and col in range(len(grid[0])) and grid[row][col] =="1":
+                    visited.add((row,col))
+                    DFS(row,col)
+                
+        
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (i,j) not in visited and grid[i][j]=="1":
+                    numberOfIslands +=1
+                    visited.add((i,j))
+                    DFS(i,j)
                     
-        
-            
+        return numberOfIslands
