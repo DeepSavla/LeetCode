4958c5f107a3bf88bd609f165c97cf1d325621e5
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-12-10 19:11:14 -0800

    Time: 8 ms (53.60%), Space: 17 MB (26.12%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -2,12 +2,12 @@ class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         num1 = int(num1)
         sum = 0
-        num2 = num2[::-1]
-        print(num2)
-        for i in range(len(num2)):
-            curVal = int(num2[i])*num1
-            if i!=0:
-                curVal = (10**i)*curVal
-            sum = sum + curVal
+        i=len(num2)-1
+        while i>=0:
+            curDigRes = int(num2[i]) * num1
+            curDigRes = curDigRes * (10**((len(num2)-1)-i))
+            print(curDigRes)
+            sum = sum+ curDigRes
+            i-=1
         return str(sum)
         
