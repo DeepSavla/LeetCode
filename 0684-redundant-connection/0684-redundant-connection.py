82c8d98103287ef6e7a4507edc396b16f0e49561
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-12-12 17:42:10 -0800

    Time: 3 ms (59.45%), Space: 17.5 MB (30.56%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,29 +1,39 @@
-#build graph one edge at a time and check if there are cycles in graph
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        def cycleExists(visited,node,source):
-            for n in adj[node]:
-                if n not in visited:
-                    visited.add(n)
-                    if cycleExists(visited,n,node):
-                        return True
-                elif n != source:
-                    return True
-            return False
-        
-        #creating adj list
-        adj = []
-        #since only one redundant edge 
-        # hence, number of nodes = number of edges
-        for i in range(len(edges)+1):
-            adj.append([])
+        # Initially each node will be its patent
+        # we have 1 to n nodes
+        parent = []
+        for i in range(len(edges) + 1):
+            parent.append(i)
+        # initially all have rank 1
+
+        rank = [1] * (len(edges) + 1)
+
+        # To find if they have same parents
+        # If yes, then they are already connected so return false
+        def find(n):
+            p = parent[n]
+            while p != parent[p]:
+                parent[p] = parent[parent[p]]
+                p = parent[p]
+            return p
+
+        # return False if cant complete union
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
+
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+    
+        # calling the union function
         for e in edges:
-            adj[e[0]].append(e[1])
-            adj[e[1]].append(e[0])
-            visited = set()
-            visited.add(e[0])
-            if cycleExists(visited,e[0],e[1]):
+            if union(e[0], e[1]) == False:
                 return e
-        
-        
-            
