dec79291f99eefcb2d92a668d5a75706a38d37a0
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2024-12-15 01:19:57 -0800

    Time: 7 ms (23.07%), Space: 17.9 MB (24.79%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,15 +3,53 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows = set()
-        cols = set()
+        firstRowHasZero = False
+        firstColHasZero = False
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if matrix[i][j]==0:
-                    rows.add(i)
-                    cols.add(j)
-        for i in range(len(matrix)):
+                if matrix[i][j] ==0:
+                    if i == 0:
+                        firstRowHasZero = True
+                    if j==0:
+                        firstColHasZero = True
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+        #do not change the first column from initial values
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+                if matrix[0][j]==0 or matrix[i][0]==0:
+                    matrix[i][j] = 0
+        #now changing first row and column
+        if firstRowHasZero:
             for j in range(len(matrix[0])):
-                if i in rows or j in cols:
-                    matrix[i][j] =0
-        return matrix
+                matrix[0][j] = 0
+        if firstColHasZero:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+'''
+for looop to check matrix
+- if any 0:
+    - Set that row and column start as zero
+Second for loop to set 0
+- if 
+
+'''
