d998c120f6d0d2b6349486a556fa1e7f14b5a192
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2025-02-16 21:22:30 -0800

    Time: 63 ms (54.72%), Space: 19.4 MB (8.33%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,19 +1,19 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def checkPalindrome(inputString, isOneDeleted):
-            l = 0
-            r = len(inputString)-1
+        def checkPalindrome(l,r, isOneDeleted):
             while l < r:
-                if inputString[l] != inputString[r]:
+                if s[l] != s[r]:
                     if isOneDeleted == True:
                         return False
                     else:
                         isOneDeleted = True
-                        return checkPalindrome(inputString[l+1:r+1], isOneDeleted) or checkPalindrome(inputString[l:r], isOneDeleted)
+                        return checkPalindrome(l+1,r, isOneDeleted) or checkPalindrome(l,r-1, isOneDeleted)
                 else:
                     l+=1
                     r-=1
             return True
 
         isOneDeleted = False
-        return checkPalindrome(s, isOneDeleted)
+        l = 0
+        r = len(s)-1
+        return checkPalindrome(l, r, isOneDeleted)
