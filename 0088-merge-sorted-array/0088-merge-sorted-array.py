e2d6fd2482aa4612dc9d2f56bd437bf818b127c6
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2025-02-17 23:10:19 -0800

    Time: 0 ms (100%), Space: 18 MB (25.81%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,21 +1,25 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        p1=m-1
-        p2=n-1
-        k=len(nums1)-1
-        while p2>=0:
-            if p1>=0 and nums1[p1]>nums2[p2]:
-                nums1[k] = nums1[p1]
-                k-=1
-                p1-=1
+        curChange = len(nums1) - 1
+        ptrM = m-1
+        ptrN = n-1
+        while ptrM >= 0 and ptrN >=0:
+            if nums1[ptrM] > nums2[ptrN]:
+                nums1[curChange] = nums1[ptrM]
+                ptrM -= 1
             else:
-                nums1[k] = nums2[p2]
-                k-=1
-                p2-=1
+                nums1[curChange] = nums2[ptrN]
+                ptrN -= 1
+            curChange -=1
+        while ptrM >=0:
+            nums1[curChange] = nums1[ptrM]
+            ptrM -= 1
+            curChange -=1
+        while ptrN >=0:
+            nums1[curChange] = nums2[ptrN]
+            ptrN -= 1
+            curChange -=1
+        return nums1
