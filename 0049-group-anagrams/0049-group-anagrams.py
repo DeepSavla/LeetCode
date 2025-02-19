ea7b79b31f4afa65040530e5de79d1d29d76e5b0
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2025-02-18 20:45:23 -0800

    Time: 27 ms (12.5%), Space: 22.9 MB (5.5%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,3 +1,7 @@
+# For each string, count how many times each character appears.
+# Convert this count array (of fixed length 26) into a tuple to use as a unique signature (hashable key).
+# Store the original string in a dictionary keyed by this signature.
+# Finally, return all the grouped strings.
 class Solution:
     def groupAnagrams(self, strs):
         """
