4aacec5c247f13cf3a3ed5a34df134f7341e50c6
DeepSavla <123166817+DeepSavla@users.noreply.github.com>
2025-02-18 20:37:51 -0800

    Time: 30 ms (10.97%), Space: 22.9 MB (5.5%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,19 @@
-class Solution(object):
+class Solution:
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        hm ={}
-        for s in strs:
-            sortedWord = sorted(s)
-            sortedWord = "".join(sortedWord)
-            if sortedWord in hm.keys():
-                hm[sortedWord].append(s)
-            else:
-                hm[sortedWord] = [s]
-        anagrams = []
-        for key in hm.keys():
-            anagrams.append(hm[key])
-        return anagrams
+        hm = {}
+        for i in strs:
+            ans = [0]*26
+            res = []
+            for c in i:
+                ans[ord(c) - ord("a")] += 1
+            if tuple(ans) not in hm:
+                hm[tuple(ans)] = [] 
+            hm[tuple(ans)].append(i) 
+        res = []
+        for k in hm.keys():
+            res.append(hm[k])
+        return res
